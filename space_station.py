d60d81670753a132a0dfc0ef6217d64a95550448
cedrichenrotel <cedric.henrotel@gmail.com>
2026-04-06 13:03:59 +0200

    ajout du typage dans les variables

--- space_station.py
+++ space_station.py
@@ -8,7 +8,7 @@
 #  By: cehenrot <cehenrot@student.42.fr>         +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/04/06 11:12:29 by cehenrot        #+#    #+#               #
-#  Updated: 2026/04/06 12:57:36 by cehenrot        ###   ########.fr        #
+#  Updated: 2026/04/06 13:03:33 by cehenrot        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -19,12 +19,14 @@ from Pydantic import BaseModel, Field, ValidatioError
 
 
 class Spacestation(BaseModel):
+    station_id: str = Field(..., min_lenght=3, max_len=10)
+    name: str = Field(..., min_lenght=1, max_len=50)
+    crew_size: str = Field(..., )
 
 
 def main() -> None:
     print("Space Station Data Validation")
     print("========================================")
-    station_id = flied
 
 
 if __name__ == "__main__":
