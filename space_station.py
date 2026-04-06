2e1826c239fca1905897c408c3e9fc49ae5e8cad
cedrichenrotel <cedric.henrotel@gmail.com>
2026-04-06 13:03:07 +0200

    avancement de l ex0

---
+++ space_station.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  space_station.py                                  :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: cehenrot <cehenrot@student.42.fr>         +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/04/06 11:12:29 by cehenrot        #+#    #+#               #
+#  Updated: 2026/04/06 12:57:36 by cehenrot        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+from datetime import datetime
+from typing import Optional
+
+from Pydantic import BaseModel, Field, ValidatioError
+
+
+class Spacestation(BaseModel):
+
+
+def main() -> None:
+    print("Space Station Data Validation")
+    print("========================================")
+    station_id = flied
+
+
+if __name__ == "__main__":
+    main()
